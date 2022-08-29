0c8b334744f323d684b2f63a16b9d0b1dd79177a
Daniel Whiffing <danielwhiffing@gmail.com>
2022-08-28 22:56:35 -0400

    adds circular/wall spawns

--- src/constants.ts
+++ src/constants.ts
@@ -1,23 +1,16 @@
 export const LEVELS = [
-  {
-    waves: [{ type: 'homer', count: 15, rate: 2000 }],
-  },
-  {
-    waves: [{ type: 'homer', count: 30, rate: 800 }],
-  },
-  {
-    waves: [{ type: 'defender', count: 8, rate: 1000 }],
-  },
+  { waves: [{ type: 'homer', count: 15, rate: 2000 }] },
+  { waves: [{ type: 'homer', count: 30, rate: 800 }] },
+  { waves: [{ type: 'defender', count: 8, rate: 1000 }] },
   {
     waves: [
-      { type: 'defender', count: 8, rate: 1000 },
+      { type: 'defender', count: 8, rate: 0 },
       { type: 'homer', count: 8, rate: 1000, delay: 5000 },
     ],
   },
   {
     waves: [
-      // should spawn in circle instead of randomly
-      { type: 'defender', count: 8, rate: 1000 },
+      { type: 'defender', count: 12, rate: 1000, circular: true },
       { type: 'homer', count: 8, rate: 1000, delay: 5000 },
     ],
   },
@@ -37,9 +30,7 @@ export const LEVELS = [
       { type: 'homer', count: 8, rate: 1000 },
     ],
   },
-  {
-    waves: [{ type: 'spike', count: 16, rate: 1000 }],
-  },
+  { waves: [{ type: 'spike', count: 16, rate: 1000 }] },
 ]
 
 interface EnemyStats {
--- src/entities/enemies.ts
+++ src/entities/enemies.ts
@@ -1,29 +1,51 @@
 import { Enemy } from './enemy'
-import { Pool, randInt } from 'kontra'
+import { movePoint, Pool, randInt } from 'kontra'
+import { ENEMY_STATS } from '../constants'
 
 const MAX_ENEMIES = 50
 export const Enemies = ({ canvas, particles }) => {
   let pool = Pool({ create: () => new Enemy(), maxSize: MAX_ENEMIES })
   return {
     pool,
-    spawn(target, { type = 'homer', count = 1, rate = 0 }) {
+    spawn(target, wave) {
+      let { type = 'homer', count = 1, rate = 0, circular, wall, dir } = wave
+      dir = dir ?? randInt(0, 3)
       for (let i = 0; i < count; i++) {
-        const b = 50
-        let dir = randInt(0, 3)
-        let x = -b,
-          y = -b
-        if (dir < 2) {
-          y = randInt(b, canvas.height - b)
-          if (dir === 1) x = canvas.width + b
+        let x, y
+
+        if (circular) {
+          const { size } = ENEMY_STATS[type]
+          const pos = movePoint(
+            {
+              x: canvas.width / 2 - size / 2,
+              y: canvas.height / 2 - size / 2,
+            },
+            -Math.PI + ((Math.PI * 2) / count) * i,
+            450,
+          )
+          x = pos.x
+          y = pos.y
         } else {
-          x = randInt(b, canvas.width - b)
-          if (dir === 3) y = canvas.height + b
+          const b = 50
+          if (!wall) dir = randInt(0, 3)
+          x = -b
+          y = -b
+          if (dir < 2) {
+            y = wall ? i * 100 : randInt(b, canvas.height - b)
+            if (dir === 1) x = canvas.width + b
+          } else {
+            x = wall ? i * 100 : randInt(b, canvas.width - b)
+            if (dir === 3) y = canvas.height + b
+          }
         }
-        setTimeout(() => {
-          const ttl = Infinity
-          const stats = { x, y, ttl, target, pool, particles, type }
-          pool.get(stats)
-        }, i * rate)
+        setTimeout(
+          () => {
+            const ttl = Infinity
+            const stats = { x, y, ttl, target, pool, particles, type }
+            pool.get(stats)
+          },
+          wall ? 0 : i * rate,
+        )
       }
     },
   }
