9c0cb43afeb6c7d8db1e5458d6d25fca6430b0ae
Daniel Whiffing <danielwhiffing@gmail.com>
2022-09-12 21:47:58 -0400

    Change upgrade costs

--- src/constants/upgrades.ts
+++ src/constants/upgrades.ts
@@ -12,40 +12,40 @@ export const UPGRADES = [
     cost: [100, 200],
   },
   {
+    description: (n: number) => `Lay mines faster`,
+    key: 'mine_speed',
+    label: 'Faster mines',
+    cost: [100, 200],
+  },
+  {
     description: (n: number) =>
       `Mines have larger explosions and do more damage`,
     key: 'mine_damage',
     label: 'Bigger mines',
-    cost: [100],
-  },
-  {
-    description: (n: number) => `Lay mines faster`,
-    key: 'mine_speed',
-    label: 'Faster mines',
-    cost: [100],
+    cost: [300],
   },
   {
     description: (n: number) => `Charge energy faster`,
     key: 'charge_speed',
     label: 'Faster charge',
-    cost: [100],
+    cost: [250, 500],
   },
   {
     description: (n: number) => `Charge more energy before max`,
     key: 'charge_max',
     label: 'More charge',
-    cost: [100],
+    cost: [300, 600],
+  },
+  {
+    description: (n: number) => `Provides shield which recharges over time.`,
+    key: 'shield',
+    label: 'Shield',
+    cost: [500],
   },
   {
     description: (n: number) => `Description`,
     key: 'bullet_count',
     label: 'More bullets',
-    cost: [100, 100],
-  },
-  {
-    description: (n: number) => `Shield`,
-    key: 'shield',
-    label: 'More shield',
-    cost: [100],
+    cost: [600, 1200],
   },
 ]
